440b140bf5147702c0be50bdef6864954ef4fb80
Adrian Kulawik <a.kulawik8@gmail.com>
2026-05-24 14:18:38 +0200

    Add map

---
+++ src/components/atoms/Map/Map.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x.src ?? markerIcon2x,
+  iconUrl: markerIcon.src ?? markerIcon,
+  shadowUrl: markerShadow.src ?? markerShadow,
+});
+
+interface MapMarker {
+  id: number | string;
+  lat: number;
+  lng: number;
+  label?: string;
+}
+
+interface MapProps {
+  center: [number, number];
+  zoom?: number;
+  markers?: MapMarker[];
+}
+
+export function Map({ center, zoom = 13, markers = [] }: MapProps) {
+  return (
+    <MapContainer
+      center={center}
+      zoom={zoom}
+      style={{ height: "100%", width: "100%" }}
+      scrollWheelZoom
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        maxZoom={19}
+      />
+
+      {markers.map((m) => (
+        <Marker key={m.id} position={[m.lat, m.lng]}>
+          {m.label && (
+            <Popup>
+              <span className="text-sm font-medium">{m.label}</span>
+            </Popup>
+          )}
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+}
