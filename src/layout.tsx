c4bca0908dd886197f69e1e0c919c101b9ae4874
Adrian Kulawik <a.kulawik8@gmail.com>
2026-05-24 14:20:12 +0200

    Make schedule page use map

--- src/layout.tsx
+++ src/layout.tsx
@@ -3,9 +3,9 @@ import { Navbar } from "@/components/navigation/Navbar";
 
 export default function MainLayout() {
   return (
-    <main className="mx-auto min-h-screen w-full max-w-7xl bg-backgroundPrimary text-contentPrimary p-2 space-y-8">
+    <main className="mx-auto flex flex-col h-screen w-full max-w-7xl bg-backgroundPrimary text-contentPrimary p-2 gap-8">
       <Navbar />
-      <div>
+      <div className="flex-1 min-h-0">
         <Outlet />
       </div>
     </main>
